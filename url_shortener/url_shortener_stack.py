12a30263a76f60d1da419dc86c49f7e799acfbaa
Siddharth Balyan <glitch@rager.local>
2022-02-13 15:19:13 +0530

    URL shortener CDK

--- url_shortener/url_shortener_stack.py
+++ url_shortener/url_shortener_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
 )
 from constructs import Construct
 
@@ -9,11 +11,23 @@ class UrlShortenerStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+    
+        table = dynamodb.Table(
+            self, "mapping-table", # scope is the construct which contains THIS construct
+            partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
+        )
 
-        # The code that defines your stack goes here
+        function = _lambda.Function(
+            self, 'backend',
+            runtime = _lambda.Runtime.PYTHON_3_9,
+            code = _lambda.Code.from_asset('./lambda'),
+            handler = 'handler.main'
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UrlShortenerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        table.grant_read_write_data(function)
+        function.add_environment("TABLE_NAME", table.table_name)
+
+        gateway = apigw.LambdaRestApi(
+            self, 'api',
+            handler = function
+        )
